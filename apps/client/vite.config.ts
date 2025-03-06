487bc7146e2f8bf20b8afd9dc306ee3a0ec8e0e7
p-alex <alex.daniel9021@gmail.com>
2025-03-06 14:04:02 +0200

    Added tailwindcss

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -3,6 +3,7 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,5 +13,5 @@ export default defineConfig({
     setupFiles: "src/setupTests.ts",
     css: true,
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 });
